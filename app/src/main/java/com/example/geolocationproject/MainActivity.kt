1ebc171a96cf1919938b8dc965050a92461902d6
Erofeev Vladislav <v.erofeev@ltst.ru>
2022-06-06 14:25:39 +0600

    VE: add getMyLocation without Permissions

--- app/src/main/java/com/example/geolocationproject/MainActivity.kt
+++ app/src/main/java/com/example/geolocationproject/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.geolocationproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geolocationproject.databinding.ActivityMainBinding
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -13,25 +15,47 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val fusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private var map: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initMap()
+        initListeners()
     }
 
     override fun onMapReady(map: GoogleMap) {
-        map.addMarker(
-            MarkerOptions().apply {
-                position(LatLng(0.0, 0.0))
-                title("Test")
-            }
-        )
+        this.map = map
     }
 
     private fun initMap() {
         (supportFragmentManager.findFragmentById(binding.map.id) as SupportMapFragment)
             .getMapAsync(this)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initListeners() {
+        binding.getMyLocationButton.setOnClickListener {
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener { lastLocationTask ->
+                if (lastLocationTask.isSuccessful) {
+                    val lastLocation = lastLocationTask.result
+                    map?.addMarker(
+                        MarkerOptions().apply {
+                            position(
+                                LatLng(lastLocation.latitude, lastLocation.longitude)
+                            )
+                        }
+                    )
+                } else {
+                    map?.clear()
+                }
+            }
+        }
     }
 }
