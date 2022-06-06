13a2f74c05e0ea39d59062c72b2937d8dbfee1be
Erofeev Vladislav <v.erofeev@ltst.ru>
2022-06-06 13:45:19 +0600

    VE: add google map fragment to MainActivity with `Test` Marker

--- app/src/main/java/com/example/geolocationproject/MainActivity.kt
+++ app/src/main/java/com/example/geolocationproject/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.geolocationproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.geolocationproject.databinding.ActivityMainBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,5 +17,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initMap()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.addMarker(
+            MarkerOptions().apply {
+                position(LatLng(0.0, 0.0))
+                title("Test")
+            }
+        )
+    }
+
+    private fun initMap() {
+        (supportFragmentManager.findFragmentById(binding.map.id) as SupportMapFragment)
+            .getMapAsync(this)
     }
 }
