e00a29b5e5d4d0e5899fc71af5b30feaf14d4942
Erofeev Vladislav <v.erofeev@ltst.ru>
2022-06-06 15:12:02 +0600

    VE: add permissions from permissions-dispatcher library

--- app/src/main/java/com/example/geolocationproject/MainActivity.kt
+++ app/src/main/java/com/example/geolocationproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.geolocationproject
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,13 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnShowRationale
+import permissions.dispatcher.PermissionRequest
+import permissions.dispatcher.RuntimePermissions
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMainBinding
@@ -39,23 +46,47 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .getMapAsync(this)
     }
 
-    @SuppressLint("MissingPermission")
     private fun initListeners() {
         binding.getMyLocationButton.setOnClickListener {
-            fusedLocationProviderClient.lastLocation.addOnCompleteListener { lastLocationTask ->
-                if (lastLocationTask.isSuccessful) {
-                    val lastLocation = lastLocationTask.result
-                    map?.addMarker(
-                        MarkerOptions().apply {
-                            position(
-                                LatLng(lastLocation.latitude, lastLocation.longitude)
-                            )
-                        }
-                    )
-                } else {
-                    map?.clear()
-                }
+            updateUserLastLocationWithPermissionCheck()
+        }
+    }
+
+    @NeedsPermission(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    @SuppressLint("MissingPermission")
+    fun updateUserLastLocation() {
+        fusedLocationProviderClient.lastLocation.addOnCompleteListener { lastLocationTask ->
+            if (lastLocationTask.isSuccessful) {
+                val lastLocation = lastLocationTask.result
+                map?.addMarker(
+                    MarkerOptions().apply {
+                        position(
+                            LatLng(lastLocation.latitude, lastLocation.longitude)
+                        )
+                    }
+                )
+            } else {
+                map?.clear()
             }
         }
+    }
+
+    @OnShowRationale(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    fun showAccessGeolocationDialog(request: PermissionRequest) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.access_geolocation_dialog_title))
+            .setPositiveButton(R.string.access_geolocation_dialog_positive_button_text) { _, _ ->
+                request.proceed()
+            }
+            .setNegativeButton(R.string.access_geolocation_dialog_negative_button_text) { _, _ ->
+                request.cancel()
+            }
+            .show()
     }
 }
